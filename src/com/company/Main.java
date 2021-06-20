a5d68a9d13139b30d3aeec066316639e8f180c8f
Kaliban-86 <koxanovskij86@mail.ru>
2021-06-20 14:38:46 +0300

    Проверка работы Git с отрпавкой в репозиторий, пока в основной ветке.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        {
-
-        }
+        int a = 10;
+        int b = 50;
+        int c = setCToFunSetting(a,b);
+        System.out.println("С = " + c);
+        System.out.println("Ура товарищи!!!!!");
 
     }
+
+    public  static int setCToFunSetting(int a, int b) {
+        int c;
+        if (a > b) {
+            c = 100;
+        } else c = 500;
+        return c;
+    }
+
 }
