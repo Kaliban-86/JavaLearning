eb6b75845e2ee5bc82609aa9bf2effa3d2461dd6
VadimIvanov <koxanovskij86@mail.ru>
2021-06-22 22:17:23 +0300

    Коммит со строками.

---
+++ src/com/company/StringTesting.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class StringTesting {
+    public static void main(String[] args) {
+
+        String stOne;
+        stOne = "One";
+
+        String stTwo = "Two";
+
+        String stThree = stOne + " " + stTwo;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(stThree);
+        }
+
+    }
+}
