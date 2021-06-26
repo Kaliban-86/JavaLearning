3429fdc076f02b714214aff4d7e0f6bb9c1b33d1
VadimIvanov <koxanovskij86@mail.ru>
2021-06-26 19:05:31 +0300

    Коммит методы, в том числе с возвращаемыми значениями, в том числе с параметрами методов.

--- src/com/company/ClassesAndObjects.java
+++ src/com/company/ClassesAndObjects.java
@@ -7,14 +7,18 @@ public class ClassesAndObjects {
         person1.age = 35;
         person1.sayHellow();
         person1.speak();
-        System.out.println("До пенсии осталось " + person1.calculateAgeTo());
+        System.out.println("До пенсии осталось " + person1.calculateAgeTo(person1.age));
 
         Person person2 = new Person();
         person2.name = "Женя";
         person2.age = 30;
         person2.sayHellow();
         person2.speak();
-        System.out.println("До пенсии осталось " + person2.calculateAgeTo());
+        System.out.println("До пенсии осталось " + person2.calculateAgeTo(person2.age));
+
+        Person person3 = new Person();
+        person3.setNameAndage(6,"Русланчик");
+        person3.speak();
 
         // System.out.println("Привет, меня зовут " + person1.name + " и мне " + person1.age + " лет!");
         //System.out.println("Привет, меня зовут " + person2.name + " и мне " + person2.age + " лет!");
@@ -26,6 +30,11 @@ class Person {
     String name;
     int age;
 
+    void setNameAndage(int userAge, String userName) {
+        name = userName;
+        age = userAge;
+    }
+
     void speak() {
         System.out.println("Привет, меня зовут " + name + " и мне " + age + " лет!");
     }
@@ -34,7 +43,7 @@ class Person {
         System.out.println("Тебе привет от " + name);
     }
 
-    int calculateAgeTo() {
-        return 60 - age;
+    int calculateAgeTo(int userAge) {
+        return 60 - userAge;
     }
 }
