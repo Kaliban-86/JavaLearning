ae6470b6cce855e5a6380334b8e1642caa88598c
VadimIvanov <koxanovskij86@mail.ru>
2021-07-01 22:27:41 +0300

    Классы обертки примитивных типов данных.

---
+++ src/com/company/СlassWrapper.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class СlassWrapper {
+
+    int x = 35;
+
+
+    Integer x1 = new Integer(36);
+
+    Integer x2 = 37;
+
+    int x4 = Integer.parseInt("125");
+
+    Integer x3 = x; // преобразование из примитива в обертку
+
+    int y = x3; // преобразование из обертки в примитив
+
+
+
+
+}
