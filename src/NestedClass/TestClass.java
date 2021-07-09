f2c4431e5f90d8e85dff83d51d1dfafa4a2bccff
VadimIvanov <koxanovskij86@mail.ru>
2021-07-10 00:17:20 +0300

    Вложенные классы: статические, нестатические и внутри метода.

--- src/NestedClass/TestClass.java
+++ src/NestedClass/TestClass.java
@@ -2,7 +2,7 @@ package NestedClass;
 
 public class TestClass {
     public static void main(String[] args) {
-        ElectroCar electroCar = new ElectroCar(35);
+        ElectroCar electroCar = new ElectroCar(38);
         electroCar.startCar();
 
         //ElectroCar.Motor motor = new ElectroCar.Motor(77);  всвязи с тем, что класс не static  не получается...
