96a7541258b4e9b0ed4484e8c6cab2f5c04eeed4
VadimIvanov <koxanovskij86@mail.ru>
2021-07-14 23:30:44 +0300

    Запись объекта в файл, и чтение из него, в том числе и массива и try with resources.

--- src/Serialazeble/ReadFile.java
+++ src/Serialazeble/ReadFile.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try {
-            FileInputStream fis = new FileInputStream("writeAndRead.bin");
-            ObjectInputStream ois = new ObjectInputStream(fis);
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("writeAndRead.bin"))) {
+            //FileInputStream fis = new FileInputStream("writeAndRead.bin"); это то что было до использования синтаксиа try with resource.
+            //ObjectInputStream ois = new ObjectInputStream(fis);
 
             Human human1 = (Human) ois.readObject();
             Human human2 = (Human) ois.readObject();
@@ -17,7 +17,7 @@ public class ReadFile {
             System.out.println(human2);
             System.out.println(Arrays.toString(people));
 
-            ois.close();
+            //ois.close();  при использовании синтаксиса try with resource  можно не закрывать поток.
 
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
--- src/Serialazeble/WriteFile.java
+++ src/Serialazeble/WriteFile.java
@@ -10,19 +10,13 @@ public class WriteFile {
     public static void main(String[] args) {
         Human human1 = new Human("Bob", 35);
         Human human2 = new Human("Tom", 37);
-        Human[] people = {new Human("Den",15),new Human("Mot",27), new
+        Human[] people = {new Human("Den", 15), new Human("Mot", 27), new
                 Human("Nina", 32)};
 
-        try {
-            FileOutputStream fos = new FileOutputStream("writeAndRead.bin");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("writeAndRead.bin"))) {
             oos.writeObject(human2); //записываем объекты в файл...
             oos.writeObject(human1);
             oos.writeObject(people);
-
-            oos.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
