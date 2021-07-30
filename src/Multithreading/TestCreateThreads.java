4d324a3374cc92c26d065a2173fac59316ed128c
VadimIvanov <koxanovskij86@mail.ru>
2021-07-30 23:56:04 +0300

    Многопоточность, создание потоков двумя способами, через класс наследуемыот от Thread, и через реализацию интерфейса Runnable. и команда sleep.

--- src/Multithreading/TestCreateThreads.java
+++ src/Multithreading/TestCreateThreads.java
@@ -1,14 +1,17 @@
 package Multithreading;
 
 public class TestCreateThreads {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // первый способ создания потоков
         MyThread myThread1 = new MyThread();
         MyThread myThread2 = new MyThread();
 
         myThread1.start(); // для запуска выполняется метод  start
-         myThread2.start();
+        myThread2.start();
+
+        Thread.sleep(1000);// команда постановки на паузу
+
 
         // второй способ создания потока
         Thread myThread3 = new Thread(new RunThread());
@@ -32,6 +35,11 @@ class RunThread implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 500; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Действие номер " + i + " но из другого потока)");
         }
     }
