b12dd7b5855b03252c08140ef372c0ed4554241e
VadimIvanov <koxanovskij86@mail.ru>
2021-08-04 11:56:14 +0300

    Методы wait() and notify() служат для низкоуровневой синхронизации раблты потоков.

---
+++ src/Multithreading/TestWaitAndNotify.java
@@ -0,0 +1,61 @@
+package Multithreading;
+
+import java.util.Scanner;
+
+public class TestWaitAndNotify {
+    public static void main(String[] args) throws InterruptedException {
+        WaitAndNotify waitAndNotify = new WaitAndNotify();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                waitAndNotify.waitTest();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    waitAndNotify.notifyTest();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+
+    }
+}
+
+class WaitAndNotify{
+
+    public void waitTest(){
+        synchronized (this){
+        System.out.println("Место в коде до запуска метода wait()");
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Место в коде после возвращения в первый поток, туда где ранее был запущен метод wait()");
+        }
+    }
+
+    public void notifyTest() throws InterruptedException {
+        Thread.sleep(2000);
+        Scanner scanner = new Scanner(System.in);
+        synchronized (this){
+            System.out.println("место в коде до запуска метода notift(), ждем нажатия кнопки Enter");
+            scanner.nextLine();
+            notify();
+            System.out.println("Метсо в коде после запуска метода notify");
+            Thread.sleep(2000);
+        }
+    }
+}
